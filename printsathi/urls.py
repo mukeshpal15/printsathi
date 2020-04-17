ba8660eab0d33bc5f109de72c6fd0c3350c5ccb8
aryankul97 <aryankul97@gmail.com>
2020-04-17 11:37:01 +0530

    aryan commit

--- printsathi/urls.py
+++ printsathi/urls.py
@@ -1,18 +1,3 @@
-"""printsathi URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from printapp.views import *
@@ -31,6 +16,13 @@ urlpatterns = [
     path('howitworks/',howitworks),
     path('pricing/',pricing),
     path('productdetails/',productdetails),
+    path('admindash/',admindash),
+    path('adminlogin/',adminlogin),
+    path('backtoadmindash/',backtoadmindash),
+    path('addproducts/',addproducts),
+    path('addpapertype/',addpapertype),
+    path('savepapertype/',savepapertype),
+    path('deletepapertype/',DeletePaperType),
     
   
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
